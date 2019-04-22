1ed66edc6dae7f41bda04370338f99057700c00c
Alisher Zakir <a3zakir@gmail.com>
2019-04-22 01:06:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,3 +20,7 @@ def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
